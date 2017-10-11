ca0821a5ec8691fe0121ac28d2547a174b7e1afd
winton <ywd979@foxmail.com>
2017-10-12 00:45:49 +0800

    注解方式springboot 缓存。

--- demo2/src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ demo2/src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -19,13 +18,13 @@ import com.example.demo2.mapper.UserMapper;
 @WebAppConfiguration
 public class Demo2ApplicationTests {
 
-	@Autowired
+	/*@Autowired
 	RedisTemplate redisTemplate;
-	
+	*/
 	@Autowired
 	UserMapper userMapper;
 	
-	@Test
+	/*@Test
 	public void contextLoads() {
 		redisTemplate.opsForValue().set("hi", "hello");
 		Assert.assertEquals("hello",redisTemplate.opsForValue().get("hi"));
@@ -34,7 +33,7 @@ public class Demo2ApplicationTests {
 	@Test
 	public void testRedisTemplateForObject() {
 		redisTemplate.opsForValue().set("hi", R.ok());
-	}
+	}*/
 	
 	/**
 	 * 测试继承mybatis
@@ -59,6 +58,34 @@ public class Demo2ApplicationTests {
 		for (User u : all) {
 			System.out.println(u.toString());
 		}
+	}
+	
+	/**
+	 * 测试Ehcache缓存
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testEhCache() throws InterruptedException {
+		System.out.println("------------------查询1--------------------");
+		List<User> all = userMapper.findById(1);
+		System.out.println(all.toString());
+		System.out.println("--------------------查询2----------------------------");
+		List<User> all2 = userMapper.findById(1);
+		System.out.println(all2.toString());
+		System.out.println("--------------------插入开始----------------------------");
+		userMapper.updateAgeById(1, 1);
+		System.out.println("--------------------插入结束----------------------------");
+		Thread.sleep(5000);
+		System.out.println("------------------查询3--------------------");
+		List<User> all3 = userMapper.findById(1);
+		System.out.println(all3.toString());
+		System.out.println("--------------------查询4----------------------------");
+		List<User> all4 = userMapper.findById(1);
+		System.out.println(all4.toString());
+		
+		
+		
+		
 	}
 
 }
